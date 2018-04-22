5bd70041a92c39478f922f671542c8469e62ff54
Mithu A Quayium <cemithu06@gmail.com>
2018-04-22 16:29:28 +0600

    Unified shortcode, features added

--- pro-demo.php
+++ pro-demo.php
@@ -1,4 +1,53 @@
 <?php
+/*add new shortcodes demo*/
+function smpro_new_shortcode_items( $items ) {
+    $items['button_group'] = array(
+        'section' => 'Content',
+        'label' => __( 'Button Group (Pro)', 'sm' ),
+        'pro' => true
+    );
+    $items['card'] = array(
+        'section' => 'Content',
+        'label' => __( 'Card (Pro)', 'sm' ),
+        'pro' => true
+    );
+    $items['card_group'] = array(
+        'section' => 'Content',
+        'label' => __( 'Card Group (Pro)', 'sm' ),
+        'pro' => true
+    );
+    $items['jumbotron'] = array(
+        'section' => 'Content',
+        'label' => __( 'Jumbotron (Pro)', 'sm' ),
+        'pro' => true
+    );
+    $items['list_group'] = array(
+        'section' => 'Content',
+        'label' => __( 'List Group (Pro)', 'sm' ),
+        'pro' => true
+    );
+    $items['carousel'] = array(
+        'section' => 'Content',
+        'label' => __( 'Carousel (Pro)', 'sm' ),
+        'pro' => true
+    );
+    return $items;
+}
+add_filter( 'smps_shortcode_items', 'smpro_new_shortcode_items' );
+
+/*tabs*/
+add_action( 'sm_bottom_settings_button_group', function () {
+    ?>
+    <p class="alert alert-danger"><?php _e( 'Pro Features', 'sm' ); ?></p>
+    <div class="form-group">
+        <label><?php _e( 'Layout Type', 'sm' ); ?></label>
+        <select class="form-control" disabled>
+            <option><?php _e( 'Select Layout Type', 'sm' ); ?></option>
+        </select>
+    </div>
+    <?php
+});
+
 /*tabs*/
 add_action( 'sm_bottom_settings_tabs', function () {
     ?>
